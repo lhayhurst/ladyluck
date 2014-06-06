9959e18f95cb9b618337b9326a9ee100d0e6b4ca
Lyle Hayhurst <sozinsky@gmail.com>
2014-06-05 23:02:59 -0500

    for game view, ordering by games in descending order by time

--- UniqueMixin.py
+++ UniqueMixin.py
@@ -5,13 +5,22 @@ def _unique(session, cls, hashfunc, queryfunc, constructor, arg, kw):
         obj = constructor(*arg, **kw)
         return obj
 
-    with session.no_autoflush:
-        q = session.query(cls)
-        q = queryfunc(q, *arg, **kw)
-        obj = q.first()
-        if not obj:
-            obj = constructor(*arg, **kw)
-            session.add(obj)
+    cache = getattr(session, '_unique_cache', None)
+    if cache is None:
+        session._unique_cache = cache = {}
+
+    key = (cls, hashfunc(*arg, **kw))
+    if key in cache:
+        return cache[key]
+    else:
+        with session.no_autoflush:
+            q = session.query(cls)
+            q = queryfunc(q, *arg, **kw)
+            obj = q.first()
+            if not obj:
+                obj = constructor(*arg, **kw)
+                session.add(obj)
+        cache[key] = obj
         return obj
 
 class UniqueMixin(object):
